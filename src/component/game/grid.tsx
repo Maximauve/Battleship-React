454ca09c7f611cc459f6dadc889437d52d7dca87
Kaly <sandra.heraud@ynov.com>
2023-12-08 09:37:11 +0100

    add Grid

--- src/component/game/grid.tsx
+++ src/component/game/grid.tsx
@@ -7,16 +7,23 @@ export interface GridProps {
 
 export const Grid: React.FC<GridProps> = ({ grid }) => {
     return (
-        <div className="grid">
-            {grid.map((row, i) => (
-                <div key={i} className="row">
-                    {row.map((cell, j) => (
-                        <div key={j} className="cell">
-                            {cell}
-                        </div>
-                    ))}
-                </div>
-            ))}
+        <div className="grid-wrapper">
+            <div className="grid playing">
+                {grid.map((row, i) => (
+                    <div key={i} className="row">
+                        {row.map((cell, j) => (
+                            <div key={j} className={ 'cell ' + cell }></div>
+                        ))}
+                    </div>
+                ))}
+            </div>
+            <div className="grid placing">
+                <div className="ship" style={{ gridRow: "1/3", gridColumn: "1" }}></div>
+                <div className="ship" style={{ gridRow: "5", gridColumn: "2/5" }}></div>
+                <div className="ship" style={{ gridRow: "10", gridColumn: "5/9" }}></div>
+                <div className="ship" style={{ gridRow: "8/10", gridColumn: "4" }}></div>
+                <div className="ship" style={{ gridRow: "5/9", gridColumn: "10" }}></div>
+            </div>
         </div>
     );
 }
