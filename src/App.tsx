56cdb0d6e2590465b53dddd83d4c3af9a2db0529
Mattox0 <matbegaaar@gmail.com>
2023-12-16 03:51:27 +0100

    Fix App CSS

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import router from 'src/config/router'
 import AppProvider from 'src/contexts/app/AppProvider'
 import UserProvider from 'src/contexts/user/UserProvider'
 import ErrorProvider from 'src/contexts/error/ErrorProvider'
+import 'src/App.css'
 
 function App() {
   return (
