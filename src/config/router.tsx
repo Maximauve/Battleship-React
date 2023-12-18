359a73225263eb001cdb40011277632ebc5c91eb
Maximauve <max.mourgues@gmail.com>
2023-12-18 23:14:54 +0100

    revert change

--- src/config/router.tsx
+++ src/config/router.tsx
@@ -31,20 +31,20 @@ const router = createBrowserRouter([
 					}
 				],
 			},
-			{
-				path: 'login',
-				element: <Login />,
-			},
-			{
-				path: 'register',
-				element: <Register />,
-			},
-			{
-				path: 'logout',
-				element: <Logout />,
-			}
 		],
 	},
+	{
+		path: '/login',
+		element: <Login />,
+	},
+	{
+		path: '/register',
+		element: <Register />,
+	},
+	{
+		path: '/logout',
+		element: <Logout />,
+	}
 ]);
 
 export default router;
